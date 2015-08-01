6b9db8fcd9576495ed438b7b65170d8f43ee500c
Markus Borg <markus.borg@gmail.com>
2015-08-01 11:00:10 +0200

    Updated Ghost class

--- app/src/main/java/com/markusborg/logic/Ghost.java
+++ app/src/main/java/com/markusborg/logic/Ghost.java
@@ -10,11 +10,13 @@ import java.util.Random;
 public class Ghost {
 
     private int speed;
+    private boolean sixPoint;
     private Random rand;
 
-    public Ghost(int iSpeed)
+    public Ghost(int iSpeed, boolean iSixPoint)
     {
         speed = iSpeed;
+        sixPoint = iSixPoint;
         rand = new Random();
     }
 
@@ -31,15 +33,60 @@ public class Ghost {
     public String nextCorner()
     {
         String corner = "";
-        switch (rand.nextInt(6))
-        {
-            case 0: corner = "L_Front"; break;
-            case 1: corner = "R_Front"; break;
-            case 2: corner = "L_Back"; break;
-            case 3: corner = "R_Back"; break;
-            case 4: corner = "L_Volley"; break;
-            case 5: corner = "R_Volley"; break;
+        if (sixPoint) {
+            switch (rand.nextInt(6)) {
+                case 0:
+                    corner = "L_FRONT";
+                    break;
+                case 1:
+                    corner = "R_FRONT";
+                    break;
+                case 2:
+                    corner = "L_BACK";
+                    break;
+                case 3:
+                    corner = "R_BACK";
+                    break;
+                case 4:
+                    corner = "L_VOLLEY";
+                    break;
+                case 5:
+                    corner = "R_VOLLEY";
+                    break;
+            }
+        }
+        else {
+            switch (rand.nextInt(4)) {
+                case 0:
+                    corner = "L_FRONT";
+                    break;
+                case 1:
+                    corner = "R_FRONT";
+                    break;
+                case 2:
+                    corner = "L_BACK";
+                    break;
+                case 3:
+                    corner = "R_BACK";
+                    break;
+            }
         }
         return corner;
+    }
+
+    public boolean isSixPoint() {
+        return sixPoint;
+    }
+
+    public void setSixPoint(boolean sixPoint) {
+        this.sixPoint = sixPoint;
+    }
+
+    public Random getRand() {
+        return rand;
+    }
+
+    public void setRand(Random rand) {
+        this.rand = rand;
     }
 }
