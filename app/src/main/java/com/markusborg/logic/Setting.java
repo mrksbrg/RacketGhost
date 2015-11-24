074fffd6139a7edf400470f4f0fb18ce4887d27a
Markus Borg <markus.borg@gmail.com>
2015-11-24 15:53:19 +0100

    Issue #27: Improved string checks

--- app/src/main/java/com/markusborg/logic/Setting.java
+++ app/src/main/java/com/markusborg/logic/Setting.java
@@ -1,6 +1,5 @@
 package com.markusborg.logic;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -39,32 +38,13 @@ public class Setting {
      */
     public Setting(String string) {
         // Multiple checks needed to take care of old app versions
-        if (string != null && string.length() > 20) {
-            date = string.substring(0, 10);
-            if (string.substring(12, 14).equals("SQ")) {
-                squash = true;
-            } else {
-                squash = false;
-            }
-            try {
-                String tempSettingSubstring = string.substring(17, string.length());
-                String[] ints = tempSettingSubstring.split(";");
-                if (ints.length == 4) {
-                    sets = Integer.parseInt(ints[0].trim());
-                    reps = Integer.parseInt(ints[1].trim());
-                    interval = Integer.parseInt(ints[2].trim());
-                    breakTime = Integer.parseInt(ints[3].trim());
-                } else {
-                    setDummyData();
-                }
-            } catch (NumberFormatException e) {
-                setDummyData();
-            } catch (ArrayIndexOutOfBoundsException e) {
-                setDummyData();
-            }
-        }
-        else {
-            setDummyData();
+        boolean validString = setSettingFromString(string);
+        // if the string is invalid, set dummy data
+        if (!validString) {
+            sets = -1;
+            reps = -1;
+            interval = -1;
+            breakTime = -1;
         }
     }
 
@@ -139,10 +119,66 @@ public class Setting {
         this.audio = audio;
     }
 
-    private void setDummyData() {
-        sets = -1;
-        reps = -1;
-        interval = -1;
-        breakTime = -1;
+    /**
+     * Validate the string, return false at earliest possible failed check. For valid string,
+     * set all important settings.
+     * @param input The string to validate and use.
+     * @return True for a valid string, otherwise false.
+     */
+    private boolean setSettingFromString(String input) {
+        // Example of valid string
+        // 2015-11-24 (SQ): 1; 1; 1; 1
+
+        // shortest possible string is 27 characters
+        if (input.length() < 27) {
+            return false;
+        }
+        // start with a date
+        String date = input.substring(0, 10);
+        if (!Character.isDigit(date.charAt(0)) ||
+                !Character.isDigit(date.charAt(1)) ||
+                !Character.isDigit(date.charAt(2)) ||
+                !Character.isDigit(date.charAt(3)) ||
+                date.charAt(4) != '-' ||
+                !Character.isDigit(date.charAt(5)) ||
+                !Character.isDigit(date.charAt(6)) ||
+                date.charAt(7) != '-' ||
+                !Character.isDigit(date.charAt(8)) ||
+                !Character.isDigit(date.charAt(9))) {
+            return false;
+        }
+        this.date = date;
+        // sport within the parantheses
+        if ((!(input.charAt(12) == 'B') && !(input.charAt(12) == 'S')) ||
+                (!(input.charAt(13) == 'A') && !(input.charAt(13) == 'Q'))) {
+            return false;
+        }
+        // if badminton, set squash to false
+        this.squash = true;
+        if (input.charAt(12) == 'B' && input.charAt(13) == 'A') {
+            this.squash = false;
+        }
+
+        // four components separated by ; in the end
+        String substring = input.substring(16, input.length());
+        String[] ints = substring.split(";");
+        if (ints.length != 4) {
+            return false;
+        }
+        // the components should be integers
+        try {
+            this.sets = Integer.parseInt(ints[0].trim());
+            this.reps = Integer.parseInt(ints[1].trim());
+            this.interval = Integer.parseInt(ints[2].trim());
+            this.breakTime = Integer.parseInt(ints[3].trim());
+        } catch (NumberFormatException e) {
+            return false;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+
+        // everything ok, the string is valid
+        return true;
     }
+
 }
