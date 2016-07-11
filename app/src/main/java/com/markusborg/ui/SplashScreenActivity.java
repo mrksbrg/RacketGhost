5f5cd6f5dc27349c13a6ace774689fda984200d2
Markus Borg <markus.borg@gmail.com>
2016-07-11 23:59:16 +0200

    Issue #47: Update build to latest API level (23) and buildToolsVersion

--- app/src/main/java/com/markusborg/ui/SplashScreenActivity.java
+++ app/src/main/java/com/markusborg/ui/SplashScreenActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 public class SplashScreenActivity extends AppCompatActivity {
 
   // Splash screen timer
-  private static int SPLASH_TIME_OUT = 5000;
+  private static int SPLASH_TIME_OUT = 4000;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
