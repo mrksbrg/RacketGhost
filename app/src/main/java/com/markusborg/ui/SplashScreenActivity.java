0ddc02cbbda5e2534f2f8531c7cfd3f2a2d8fe1b
Markus Borg <markus.borg@gmail.com>
2016-07-11 23:32:22 +0200

    Issue #49: Add a splash screen

---
+++ app/src/main/java/com/markusborg/ui/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.markusborg.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+  // Splash screen timer
+  private static int SPLASH_TIME_OUT = 5000;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash_screen);
+
+    new Handler().postDelayed(new Runnable() {
+
+      @Override
+      public void run() {
+        // Starting app main activity after time out
+        Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(i);
+        // close this activity
+        finish();
+      }
+    }, SPLASH_TIME_OUT);
+  }
+
+}
